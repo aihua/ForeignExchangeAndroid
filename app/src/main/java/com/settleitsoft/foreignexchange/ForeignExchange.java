b9e2d431d1649e1f34699a194a1d21cac42aaf30
Fernando <usuario2.settleitsoft@gmail.com>
2018-04-10 08:01:53 -0500

    Actualizacion de la App #4

--- app/src/main/java/com/settleitsoft/foreignexchange/ForeignExchange.java
+++ app/src/main/java/com/settleitsoft/foreignexchange/ForeignExchange.java
@@ -5,21 +5,33 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class ForeignExchange extends AppCompatActivity {
+
+    private Fragment welcomeFragment;
+    private Toolbar toolbar;
+    private TextView toolbarTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foreign_exchange);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Configura el Toolbar
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        Fragment i =new FragmentWelcome();
+        // Configura titulo del toolbar
+        toolbarTitle = findViewById(R.id.toolbar_Text);
+        toolbarTitle.setText(R.string.welcome_title);
+
+        // Obtiene el fragment de Welcome
+        welcomeFragment = new FragmentWelcome();
 
         FragmentTransaction fragTransaction = getSupportFragmentManager().beginTransaction();
-        fragTransaction.replace(R.id.fragm_container, i );
+        fragTransaction.replace(R.id.fragm_main_container, welcomeFragment );
         fragTransaction.commit();
     }
 }
