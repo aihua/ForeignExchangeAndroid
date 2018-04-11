5909ed839003eed6ce6a7c50654ac440a39388eb
Fernando <usuario2.settleitsoft@gmail.com>
2018-04-11 11:48:33 -0500

    Actualizacion de la App #5.3

--- app/src/main/java/com/settleitsoft/foreignexchange/FragmentTravelItinerary.java
+++ app/src/main/java/com/settleitsoft/foreignexchange/FragmentTravelItinerary.java
@@ -5,11 +5,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Spinner;
+import android.widget.EditText;
 
 public class FragmentTravelItinerary extends Fragment {
 
     private View travelItineraryLayout;
+    private EditText departureDate, returnDate;
+    private EmbeddedDatepicker departureDatepicker, returnDatepicker;
 
     // Metodo principal o constructor
     @Override
@@ -17,7 +19,39 @@ public class FragmentTravelItinerary extends Fragment {
                              Bundle savedInstanceState) {
 
         // Obtiene items del diseño
-        travelItineraryLayout  = inflater.inflate(R.layout.fragment_travel_itinerary, container, false);
+        travelItineraryLayout   = inflater.inflate(R.layout.fragment_travel_itinerary, container, false);
+        departureDate           = travelItineraryLayout.findViewById(R.id.departureDate_Edit);
+        returnDate              = travelItineraryLayout.findViewById(R.id.returnDate_Edit);
+
+        // Instancia los objetos EmbeddedDatepicker correspondiente a cada EditText
+        departureDatepicker  = new EmbeddedDatepicker(getActivity(), departureDate);
+        returnDatepicker     = new EmbeddedDatepicker(getActivity(), returnDate);
+
+        // Configura los eventos de escucha
+        listenerEditTextSetup();
+
         return travelItineraryLayout;
+    }
+
+    /* Metodo que configura los eventos de escucha de los
+     * EditText correspondientes.
+     */
+    private void listenerEditTextSetup(){
+
+        // Declara el evento de escucha del item EditText departureDate_Edit
+        departureDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                departureDatepicker.show();
+            }
+        });
+
+        // Declara el evento de escucha del item EditText returnDate_Edit
+        returnDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnDatepicker.show();
+            }
+        });
     }
 }
